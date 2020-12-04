31c59de27147090bcad2a1e70884ea03d13a3a4e
Cameron Abbot <cpabbot1@gmail.com>
2020-12-04 14:08:37 -0500

    fix

--- chat_classification.py
+++ chat_classification.py
@@ -34,7 +34,8 @@ xml_path = st.text_input("URL path to xml data file", "")
 # Prevents running errors before user has entered data file URL
 if(xml_path == ""):
     st.write("No data entered.")
-    quit()
+    import sys
+    sys.exit()
 
 try:
     df_sms = pdx.read_xml("https://drive.google.com/uc?id=" + xml_path, encoding="utf8")
